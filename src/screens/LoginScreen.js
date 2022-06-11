9c1e8f286b6092d5bd338d596027bb06452aecca
Jeremy F McKay <jeremyfmckay@gmail.com>
2022-06-11 07:24:53 -0700

    Styling log in screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,21 @@
 // This is where the login stuff will go I'll create some dummy before adding
 // real authentication
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
   return (
     <View style={styles.viewStyles}>
+        <TextInput style={styles.inputStyle}
+            placeholder='Username' 
+            autoCapitalize='none'
+            autoCorrect={false}    
+        />
+        <TextInput style={styles.inputStyle}
+            placeholder='Password' 
+            autoCapitalize='none'
+            autoCorrect={false}  
+        />
         <TouchableOpacity onPress={() => console.log("Touchable opacity pressed")}>
             <View style={styles.textOpacityStyle}>
                 <Text style={styles.textStyle}>
@@ -22,7 +32,7 @@ export default LoginScreen
 
 const styles = StyleSheet.create({
     viewStyles: {
-        backgroundColor: 'red',
+        backgroundColor: 'green',
         flexDirection: 'column',
         justifyContent: "center",
         alignItems: 'center',
@@ -32,14 +42,20 @@ const styles = StyleSheet.create({
         backgroundColor: 'black',
         width: 250,
         height: 100,
-        color: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
     }, 
     textStyle: {
         color: 'white',
         backgroundColor: 'black',
-        width: '100%',
+        paddingHorizontal: 40,
+        paddingVertical: 15,
+        borderRadius: 5,
+    },
+    inputStyle: {
+        backgroundColor: '#fefefe',
+        borderRadius: 5,
+        margin: 5,
+        padding: 10,
+        fontSize: 18,
     }
 
 })
